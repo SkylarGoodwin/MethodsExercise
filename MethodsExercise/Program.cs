0d3c7dabc7256e68933802def30ce3a970cad494
SkylarGoodwin <JamesSkylarGoodwin@gmail.com>
2024-02-05 02:21:43 -0600

    summand and factor

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace MethodsExercise
+﻿internal class Program
 {
-    public class Program
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine("What is your name?");
+        string playerName = Console.ReadLine();
+        Console.WriteLine("Hello, ", playerName,
+        ". what were you looking to purchase today?");
+        string goalItem = Console.ReadLine();
+        Console.WriteLine("A ", goalItem,
+        "Eh?, an adventurer of taste");
+    }
+    public static int Sum( int summand1, int summand2)
+    {
+        int sum = summand1 + summand2;
+        return sum;
+    }
+    public static int Multiply(int factor1, int factor2)
+    {
+        int product = factor1 * factor2;
+        return product;
     }
 }
