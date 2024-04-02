2543a1adaa468bd04460cae83409a1ae356b300d
SkylarGoodwin <JamesSkylarGoodwin@gmail.com>
2024-04-01 19:57:32 -0500

    added writelines for +X in main method

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,4 +1,6 @@
-﻿internal class Program
+﻿using System.Dynamic;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
@@ -9,6 +11,11 @@
         string goalItem = Console.ReadLine();
         Console.WriteLine("A " + goalItem +
         " Eh?, an adventurer of taste");
+
+        var num1 = GetUserNumber();
+        var num2 = GetUserNumber();
+        Console.WriteLine($"the sum of {num1} and {num2} is {Sum (num1, num2)}");
+        Console.WriteLine($"the product of {num1} and {num2} is {Multiply(num1, num2)}");
     }
     public static int Sum( int summand1, int summand2)
     {
@@ -20,4 +27,17 @@
         int product = factor1 * factor2;
         return product;
     }
+    static int GetUserNumber()
+    {
+        Console.WriteLine("enter a number");
+        var input = Console.ReadLine();
+        var isNumber =int.TryParse(input, out var number);
+        if (isNumber)
+        {
+            return number;
+        }
+        Console.WriteLine("Invalid Input");
+        return GetUserNumber();
+    }
+  
 }
